640a9c5dd69684ae50f04862023e595f5151d085
adile_app <adileshemshedinovaa@gmail.com>
2023-02-14 16:00:16 +0200

    Playwright python 09 Test Project

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # Repository for playwright_tests
+
+## Install guide
+...
+
+## Run guide
+...
+
+## Useful links
+ - https://playwright.dev/
+ - https://github.com.microsoft/playwright-python
--- conftest.py
+++ conftest.py
@@ -1,11 +1,16 @@
 from pytest import fixture
+from playwright.sync_api import sync_playwright
+from page_objects.application import App
 
 
 @fixture
-def ultimate_answer():
-    return 42
+def get_playwright():
+    with sync_playwright() as playwright:
+        yield playwright
 
 
 @fixture
-def new_answer(ultimate_answer):
-    return ultimate_answer + 1
+def desktop_app(get_playwright):
+    app = App(get_playwright)
+    yield app
+    app.close()

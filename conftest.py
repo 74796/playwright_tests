3e7bd30e3d168caa40006134dd54f3083fd3ab56
adile_app <adileshemshedinovaa@gmail.com>
2023-02-18 15:51:54 +0200

    Playwright python 12 Configurations

--- conftest.py
+++ conftest.py
@@ -1,13 +1,16 @@
+import os
+import json
 from pytest import fixture
 from playwright.sync_api import sync_playwright
 from page_objects.application import App
+import settings
 
 
 @fixture(autouse=True, scope='function')
 def preconditions():
-    print('setup preconditions state')
+    print('hello')
     yield
-    print('setup postconditions state')
+    print('world')
 
 
 @fixture(scope='session')
@@ -17,16 +20,30 @@ def get_playwright():
 
 
 @fixture(scope='session')
-def desktop_app(get_playwright):
-    app = App(get_playwright, base_url='http://127.0.0.1:8000')
+def desktop_app(get_playwright, request):
+    base_url = request.config.getoption('--base_url')
+    app = App(get_playwright, base_url=base_url)
     app.goto('/')
     yield app
     app.close()
 
 
 @fixture(scope='session')
-def desktop_app_auth(desktop_app):
-    app = desktop_app
-    app.goto('/login')
-    app.login('alice', 'Qamania123')
-    yield app
+def desktop_app_auth(desktop_app, request):
+    secure = request.config.getoption('--secure')
+    config = load_config(secure)
+    desktop_app.goto('/login')
+    desktop_app.login(**config)
+    yield desktop_app
+
+
+def pytest_addoption(parser):
+    parser.addoption('--base_url', action='store', default='http://127.0.0.1:8000')
+    parser.addoption('--secure', action='store', default='secure.json')
+    #parser.addini('base_url', help='base url of site under test', default='http://127.0.0.1:8000')
+
+
+def load_config(file):
+    config_file = os.path.join(os.path.dirname(os.path.abspath(__file__)), file)
+    with open(config_file) as cfg:
+        return json.loads(cfg.read())
